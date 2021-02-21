d7a266bb2ba0e73990c6c775d9392be0846a1535
viniciusribeiroads <viniciusribeiro113@gmail.com>
2021-02-21 19:42:18 -0300

    Classe aluno criada, adicionar aluno e listar alunos implementadas

--- Program.cs
+++ Program.cs
@@ -6,14 +6,9 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a opção desejada: ");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Lista alunos");
-            Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine("X - Sair");
-            Console.WriteLine();
-
-            string opcaoUsuario = Console.ReadLine();
+            Aluno [] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
             {
@@ -21,11 +16,35 @@ namespace Revisao
                 {
                     case "1":
                         //adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal!");
+                        }
+
+                        alunos [indiceAluno] = aluno;
+                        indiceAluno++;
+                        
 
                         break;
                     case "2":
                         //listar alunos
-
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                            Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
                         break;
                     case "3":
                         //calcular media geral
@@ -38,17 +57,24 @@ namespace Revisao
 
                 }
 
-                Console.WriteLine("Informe a opção desejada: ");
-                Console.WriteLine("1 - Inserir novo aluno");
-                Console.WriteLine("2 - Lista alunos");
-                Console.WriteLine("3 - Calcular média geral");
-                Console.WriteLine("X - Sair");
-                Console.WriteLine();
-
-                opcaoUsuario = Console.ReadLine();
+                opcaoUsuario = ObterOpcaoUsuario();
 
             }
 
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Lista alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
