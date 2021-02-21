7af4c7b791b52cb147b86ea76d946887b8c611ed
viniciusribeiroads <viniciusribeiro113@gmail.com>
2021-02-21 18:55:32 -0300

    Criação do meu primeiro projeto c#

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Lista alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //adicionar aluno
+
+                        break;
+                    case "2":
+                        //listar alunos
+
+                        break;
+                    case "3":
+                        //calcular media geral
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+
+
+                }
+
+                Console.WriteLine("Informe a opção desejada: ");
+                Console.WriteLine("1 - Inserir novo aluno");
+                Console.WriteLine("2 - Lista alunos");
+                Console.WriteLine("3 - Calcular média geral");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
